0432cbf807a235fe7304505d909c59a5f4f35c1e
Bradley Lebetkin <bradleylebetkin@Bradleys-Air.home>
2017-08-04 01:13:36 +0100

    created game logic 3

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
  console.log("Up and running!");
 
-   var cardOne = "queen";
-   var cardTwo = "queen";
-   var cardThree = "king";
-   var cardFour = "king";
+   var cards = ["queen", "queen", "king", "king"];
+   var cardsInPlay = [];
+   var CardOne = [0];
+   cardsInPlay.push(CardOne);
+   console.log("User flipped queen");
+   var cardFour = [3];
+   cardsInPlay.push(cardFour);
+   console.log("User flipped king");
+   if(cardsInPlay.length === 3 && cardsInPlay[0] === cardsInPlay[3]){
+   	alert("You have found a match");
+   }
+   else
+   {
+   	alert("soz, try again");
+   }
 
-     console.log("User flipped " + cardOne);
 
-     console.log("User flipped " + cardFour);
 
      //testy test
